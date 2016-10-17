89daa92ae189a66212d5cf4e5eac58df58f76b5a
An Nguyen <francisnguyen1201@gmail.com>
2016-10-17 12:12:12 -0400

    Not needed anymoar

--- untitled.php
+++
@@ -1,8 +0,0 @@
-$matches = $_GET['matches'];
-$acct = $_GET['accountid'];
-$APIKEY =;
-
-$steamurl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=JSON&key=$APIKEY&account_id=76561198012171408&matches_requested=5"
-$json_object= file_get_contents($steamurl);
-header('Content-Type: application/json');
-echo $json_object;
