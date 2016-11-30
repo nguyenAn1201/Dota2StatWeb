0d8fe39146d421a91767497281c08bb0d99bc79a
An Nguyen <francisnguyen1201@gmail.com>
2016-11-30 13:28:09 -0500

    my first commit

--- getMatch.js
+++ getMatch.js
@@ -1,43 +1,57 @@
 $(document).ready(function() {
-  $('#myForm').submit(function(e) {
-    e.preventDefault();  
 
-  var search_input = $('#search-bar').val();
-  console.log(search_input);
+  $('#myForm').submit(function(e) {
+    e.preventDefault();
+  //jQuery('body').css('background-image', 'url("http://3.bp.blogspot.com/-4ihW7h8hTr8/UA6iCeyA8oI/AAAAAAAAAuw/xXDlVQdEWN4/s1600/dota-2-wallpaper-4.jpg")');
+  var search_input = $('#search-bar').val(); 
+
+  $.getJSON("https://api.opendota.com/api/players/" + search_input, function(data) {
+    $('.playerProfile').empty().append(
+        '<div id="playerProfile" class="text-xs-center" style="margin-top: 5%; padding:10px; background-color: #34495E">' +
+        '<img src="' + data.profile.avatarmedium + '">' +
+        '<h5 style="color: white"><a href="' + data.profile.profileurl + '">' + data.profile.personaname + '</a></h5>' + 
+        '<ul class="list-unstyled">' + '</li>' +
+        '<li style="color: white">Estimated MMR ' + '</li>' +
+        '<li style="color: white">' + data.mmr_estimate.estimate + '</li>' +
+      '</ul>' + '</div>'
+
+    );
+  });
+
+  $.getJSON("https://api.opendota.com/api/players/" + search_input+ "/wl", function(data) {
+        
+    $('#playerProfile').append(
+      '<p style="margin-bottom:-5px ;color: white"> Win - Loss</p>' +
+      '<span style="color: green">' + data.win + 
+      '</span>' + '<span style="color: white"> - </>' + '<span style="color: red">' + data.lose + '</>'
+    );
+  });
+
   $.getJSON("https://api.opendota.com/api/players/" +search_input+ "/matches", function(data) {
 
-  
-    $('#result').append(
-      '<table class="table table-inverse table-striped table-bordered table-hover">' +
-      '<thead>' +
-        '<th class="text-xs-center">Hero</th>' +
-        '<th class="text-xs-center">Duration</th>' +
-        '<th class="text-xs-center">Result</th>' +
-        '<th class="text-xs-center">Type</th>' +
-        '<th class="text-xs-center">KDA</th>' +
-      '</thhead>' +
-      '<tbody class="matches">' + 
-      '</tbody>'
-    );
-
-/*
-      $('.matches').append(
-        '<tr>' +
-        '<td class="text-xs-center" id="image">' + '</td>' +
-        '<td class="text-xs-center">' + secondsTimeSpanToHMS(data[i].duration) + '</td>' +
-        winloss(i) +  
-        type(i) + 
-        '<td>' + data[i].kills + "/" + data[i].deaths + "/" + data[i].assists + '</td>' +
-        '</tr>'
-      );
-      */
-    
-    for (var i=0; i<10; i++) {
-      
-      getPortrait(data[i].hero_id, i);
-
-     
+    console.log(data.length);
+    if (data.length == 0)
+    {
+      alert("Please input a correct 9-digit SteamId")
+      location.reload();
     }
+      $('#result').empty().append(
+        '<table id="myTable"  class="table table-inverse table-striped table-bordered table-hover table-responsive">' +
+        '<thead>' +
+          '<th class="text-xs-center">Hero</th>' +
+          '<th class="text-xs-center">Duration</th>' +
+          '<th class="text-xs-center">Result</th>' +
+          '<th class="text-xs-center"><abbr title="Game Type">Type</abbr></th>' +
+          '<th class="text-xs-center"><abbr title="Kill/Death/Assist">KDA</abbr></th>' +
+        '</thhead>' +
+        '<tbody id="matches">' + 
+        '</tbody>'
+      );  
+
+      for (var i=0; i<100; i++) {
+        getPortrait(data[i].hero_id, i);
+      }
+    
 
     function winloss(i) {
       if (data[i].radiant_win==true && data[i].player_slot <128) {
@@ -70,16 +84,23 @@ $(document).ready(function() {
 
     function getPortrait(i, u ){
       $.getJSON("https://raw.githubusercontent.com/odota/dotaconstants/master/json/heroes.json", function(herodata) {
-        $('.matches').append(
-          '<tr>' +
-          '<td><img style="width: 30%; height: 20%; vertical-align:middle" src=http://cdn.dota2.com' + herodata[i].img + '>' + '<span class="small"> ' + herodata[i].localized_name + '</span></td>' +  '</td>'  + '<td>' + secondsTimeSpanToHMS(data[i].duration) + '</td>' + winloss(u) + type(u) + '<td>' + data[u].kills + "/" + data[u].deaths + "/" + data[u].assists + '</td>' +
+        $('#matches').append(
+          '<tr id="r1">' +
+          '<td><img style="width: 30%; height: 20%; vertical-align:middle" src=http://cdn.dota2.com' + 
+          herodata[i].img + '>' + 
+          '<span class="small">' +
+          '<a href="http://localhost/Doto2ez/matchPage.php?matchId=' + data[u].match_id + '"</a>' + herodata[i].localized_name + '</span></td>' + 
+          '<td>' + secondsTimeSpanToHMS(data[u].duration) + '</td>' + 
+          winloss(u) + 
+          type(u) + 
+          '<td>' + data[u].kills + "/" + data[u].deaths + "/" + data[u].assists + '</td>' +
           '</tr>' 
           );
       });
     }
-
-
     });
-  });
-});
 
+  });
+  
+
+});
--- matchPage.php
+++ matchPage.php
@@ -8,24 +8,27 @@
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="sha384-2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="sha384-VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script src="matchStat.js"></script>
-	
+		<script>
+			var dismatchId = '<?php echo $_GET["matchId"]?>';
+		</script>
 	</head>
 	<body style="background-color: #141414">
-
+	
 	<div class="container" style="width: 80%; margin-top: 50px;">
 		<div class="row">
 			<div class="col-md-6">
-				<h2 style="color: #EBEBEB">Match 2756880995</h2><br/>
+				<h2 id="matchId" style="color: #EBEBEB">Match </h2><br/>
 				<h4 style="margin-top: -25px; color: #EBEBEB"><i>Overview</i></h4>
 			</div>
 			<div class="col-md-6">
 				<table class="table table-inverse small">
 					<thead>
-						<th>Mode</th><th>Duration</th><th>Skill</th></tr>
+						<th class="text-xs-center">Mode</th><th class="text-xs-center">Duration</th><th class="text-xs-center">Skill</th></tr>
 					</thead>
 					<tbody>
-						<tr><td>All Pick</td><td>0:42:43</td><td>Normal</td></tr>
+						<tr><td class="text-xs-center">All Pick</td><td class="text-xs-center" id="duration"></td><td class="text-xs-center" id="skill"></td></tr>
 					</tbody>
 				</table>
 			</div>	
@@ -44,6 +47,38 @@
 				
 			</div>
 		</div>
+
+		<!-- Chart -->
+		<!--
+		<table class="columns">
+      	
+        	<td><div id="columnchart_div"></div></td>
+        	<td><div id="columnchart_div2"></div></td>
+
+    	</table>
+    	-->		
+    	<table class="columns">
+      		<tr>
+        		<td><div id="columnchart_div" style="border: 1px solid #ccc"></div></td>
+        	</tr>
+        	<tr>
+        		<td><div id="columnchart_div2" style="border: 1px solid #ccc"></div></td>
+      		</tr>
+      	</table>
+      	<table>
+      		<tr>
+        		<td><div id="piechart1" style="border: 1px solid #ccc"></div></td>
+        		<td><div id="piechart2" style="border: 1px solid #ccc"></div></td>
+        		
+      		</tr>
+      	</table>
+      	<table>
+      		<tr>
+      			<td>
+      				<div id="piechart3" style="border: 1px solid #ccc"></div>
+      			</td>
+      		</tr>
+    	</table>
 	</div> <!--container-->
 
 		
